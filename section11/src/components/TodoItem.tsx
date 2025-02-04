2794b6985206901a1df0aedff70e7043fc5eb58d
kmw <skqjahjakskxl@gmail.com>
2025-02-05 01:07:05 +0900

    11번째 수업

---
+++ section11/src/components/TodoItem.tsx
@@ -0,0 +1,22 @@
+import { useTodoDispatch } from "../App";
+import { Todo } from "../types";
+
+interface Props extends Todo {
+}
+
+function TodoItem(props: Props) {
+
+  const dispatch = useTodoDispatch()
+
+  const onClickButton = () => {
+    dispatch.onClickDelete(props.id);
+  };
+  return (
+    <div>
+      {props.id}번 : {props.content}
+      <button onClick={onClickButton}>삭제</button>
+    </div>
+  );
+}
+
+export default TodoItem;
